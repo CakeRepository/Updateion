179cb111f111bc72e10238361a5a6162f5deedea
Justin Trantham <takescake@outlook.com>
2017-05-09 13:29:17 -0600

    Init

---
+++ updaterionv.3/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace updaterionv._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.driverCheckBox = new System.Windows.Forms.CheckBox();
+            this.softwareCheckBox = new System.Windows.Forms.CheckBox();
+            this.scanButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.errorBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 142);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(267, 199);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // driverCheckBox
+            // 
+            this.driverCheckBox.AutoSize = true;
+            this.driverCheckBox.Location = new System.Drawing.Point(12, 13);
+            this.driverCheckBox.Name = "driverCheckBox";
+            this.driverCheckBox.Size = new System.Drawing.Size(97, 17);
+            this.driverCheckBox.TabIndex = 1;
+            this.driverCheckBox.Text = "Driver Updates";
+            this.driverCheckBox.UseVisualStyleBackColor = true;
+            this.driverCheckBox.CheckedChanged += new System.EventHandler(this.driverCheckBox_CheckedChanged);
+            // 
+            // softwareCheckBox
+            // 
+            this.softwareCheckBox.AutoSize = true;
+            this.softwareCheckBox.Location = new System.Drawing.Point(12, 37);
+            this.softwareCheckBox.Name = "softwareCheckBox";
+            this.softwareCheckBox.Size = new System.Drawing.Size(111, 17);
+            this.softwareCheckBox.TabIndex = 2;
+            this.softwareCheckBox.Text = "Software Updates";
+            this.softwareCheckBox.UseVisualStyleBackColor = true;
+            this.softwareCheckBox.CheckedChanged += new System.EventHandler(this.softwareCheckBox_CheckedChanged);
+            // 
+            // scanButton
+            // 
+            this.scanButton.Location = new System.Drawing.Point(12, 60);
+            this.scanButton.Name = "scanButton";
+            this.scanButton.Size = new System.Drawing.Size(266, 53);
+            this.scanButton.TabIndex = 3;
+            this.scanButton.Text = "Scan";
+            this.scanButton.UseVisualStyleBackColor = true;
+            this.scanButton.Click += new System.EventHandler(this.scanButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(189, 13);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(90, 23);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // errorBox1
+            // 
+            this.errorBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.errorBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.errorBox1.ForeColor = System.Drawing.Color.Red;
+            this.errorBox1.Location = new System.Drawing.Point(47, 119);
+            this.errorBox1.Name = "errorBox1";
+            this.errorBox1.Size = new System.Drawing.Size(231, 13);
+            this.errorBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 366);
+            this.Controls.Add(this.errorBox1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.scanButton);
+            this.Controls.Add(this.softwareCheckBox);
+            this.Controls.Add(this.driverCheckBox);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckBox driverCheckBox;
+        private System.Windows.Forms.CheckBox softwareCheckBox;
+        private System.Windows.Forms.Button scanButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.TextBox errorBox1;
+    }
+}
+
---
+++ updaterionv.3/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WUApiLib;
+
+namespace updaterionv._3
+{
+    public partial class Form1 : Form
+    {
+        int scanCounter = 0;
+        public List<string> MyList { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scanButton_Click(object sender, EventArgs e)
+        {
+            if (scanCounter == 0)
+            {
+                scanCounter++;
+                errorBox1.Text = "";
+                MyList = new List<string>();
+                var bw = new BackgroundWorker();
+                bw.DoWork += (o, args) => scanWorker();
+                bw.RunWorkerCompleted += (o, args) => MethodToUpdateControl();
+                bw.RunWorkerAsync();
+                
+            }
+            else
+            {
+                errorBox1.Text = "Please press clear before rescanning.";
+            }
+            
+        }
+        private void MethodToUpdateControl()
+        {
+            // since the BackgroundWorker is designed to use
+            // the form's UI thread on the RunWorkerCompleted
+            // event, you should just be able to add the items
+            // to the list box:
+            checkedListBox1.Items.AddRange(MyList.ToArray());
+
+            // the above should not block the UI, if it does
+            // due to some other code, then use the ListBox's
+            // Invoke method:
+            // listBox1.Invoke( new Action( () => listBox1.Items.AddRange( MyList.ToArray() ) ) );
+        }
+        void scanWorker()
+        {
+            
+            UpdateSession uSession = new UpdateSession();
+            IUpdateSearcher uSearcher = uSession.CreateUpdateSearcher();
+
+            if (driverCheckBox.Checked)
+            {
+                /*
+                 * 
+                 */
+                ISearchResult dResult = uSearcher.Search("IsInstalled=0 and Type='Driver'");
+                foreach (IUpdate update in dResult.Updates)
+                {
+                    if (update == null)
+                    {
+
+                    }
+                    else if (!MyList.Contains(update.Title))
+                    {
+                        MyList.Add(update.Title);
+                    }
+
+
+                }
+            }
+            if (softwareCheckBox.Checked)
+            {
+                ISearchResult uResult = uSearcher.Search("IsInstalled=0 and Type='Software'");
+                foreach (IUpdate update in uResult.Updates)
+                {
+                    if (update == null)
+                    {
+                        MyList.Add("no updates found");
+                    }
+                    if (!MyList.Contains(update.Title))
+                    {
+                        MyList.Add(update.Title);
+                    }
+
+                }
+
+            }
+
+            
+        }
+
+        void diWorker()
+        {
+            UpdateSession uSession = new UpdateSession();
+            IUpdateSearcher uSearcher = uSession.CreateUpdateSearcher();
+            ISearchResult uResult = uSearcher.Search("IsInstalled=0 and Type='Software'");
+
+            UpdateDownloader downloader = uSession.CreateUpdateDownloader();
+            downloader.Updates = uResult.Updates;
+            downloader.Download();
+            UpdateCollection updatesToInstall = new UpdateCollection();
+            foreach (IUpdate update in uResult.Updates)
+            {
+                if (update.IsDownloaded)
+                    updatesToInstall.Add(update);
+            }
+            IUpdateInstaller installer = uSession.CreateUpdateInstaller();
+            installer.Updates = updatesToInstall;
+            IInstallationResult installationRes = installer.Install();
+            for (int i = 0; i < updatesToInstall.Count; i++)
+            {
+                if (installationRes.GetUpdateResult(i).HResult == 0)
+                {
+                    Console.WriteLine("Installed : " + updatesToInstall[i].Title);
+                }
+                else
+                {
+                    Console.WriteLine("Failed : " + updatesToInstall[i].Title);
+                }
+            }
+        }
+
+        private void driverCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void softwareCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Clear();
+            scanCounter = 0;
+        }
+    }
+}
